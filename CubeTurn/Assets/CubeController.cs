d0158144bf5f9942a7214ea23b0c356e07527386
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-12-18 11:39:27 +0100

    UDLRFB added to objects

--- CubeTurn/Assets/CubeController.cs
+++ CubeTurn/Assets/CubeController.cs
@@ -3,10 +3,39 @@ using UnityEngine.UI;
 
 public class CubeController : MonoBehaviour
 {
+    public Material red;
+    public Material green;
+    public Material blue;
+    public Material yellow;
+    public Material orange;
+    public Material white;
+
+    public Material[] uMap = new Material[9];
+    public Material[] dMap = new Material[9];
+    public Material[] fMap = new Material[9];
+    public Material[] bMap = new Material[9];
+    public Material[] lMap = new Material[9];
+    public Material[] rMap = new Material[9];
+   
+    public MeshRenderer[] down;
+    public MeshRenderer[] up;
+    public MeshRenderer[] right;
+    public MeshRenderer[] left;
+    public MeshRenderer[] front;
+    public MeshRenderer[] back;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < 9; i++)
+        {
+            uMap[i] = yellow;
+            dMap[i] = white;
+            fMap[i] = blue;
+            bMap[i] = green;
+            lMap[i] = orange;
+            rMap[i] = red;
+        }
     }
 
     // Update is called once per frame
