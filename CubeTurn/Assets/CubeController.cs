639e0191a03aed2bb726361a11a89c1a825bfb36
dDreistein <178164238+dDreistein@users.noreply.github.com>
2024-12-19 09:32:34 +0100

    Made Cube Prefab and added E, U, EPrime, UPrime

--- CubeTurn/Assets/CubeController.cs
+++ CubeTurn/Assets/CubeController.cs
@@ -18,21 +18,96 @@ public class CubeController : MonoBehaviour
     Material[] bMap = new Material[9];
     Material[] lMap = new Material[9];
     Material[] rMap = new Material[9];
-   
-    public MeshRenderer[] down = new MeshRenderer[9];
-    public MeshRenderer[] up = new MeshRenderer[9];
-    public MeshRenderer[] right = new MeshRenderer[9];
-    public MeshRenderer[] left = new MeshRenderer[9];
-    public MeshRenderer[] front = new MeshRenderer[9];
-    public MeshRenderer[] back = new MeshRenderer[9];
+
+    MeshRenderer[] down = new MeshRenderer[9];
+    MeshRenderer[] up = new MeshRenderer[9];
+    MeshRenderer[] right = new MeshRenderer[9];
+    MeshRenderer[] left = new MeshRenderer[9];
+    MeshRenderer[] front = new MeshRenderer[9];
+    MeshRenderer[] back = new MeshRenderer[9];
     
     InputAction rightUp, rightDown, leftUp, leftDown, r1, r2, r3, l1, l2, l3, x, xPrime, y, yPrime, z ,zPrime;
-
-    public bool working;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        up = new []
+        {
+            GameObject.Find("Up1").GetComponent<MeshRenderer>(), 
+            GameObject.Find("Up2").GetComponent<MeshRenderer>(), 
+            GameObject.Find("Up3").GetComponent<MeshRenderer>(),
+            GameObject.Find("Up4").GetComponent<MeshRenderer>(), 
+            GameObject.Find("Up5").GetComponent<MeshRenderer>(), 
+            GameObject.Find("Up6").GetComponent<MeshRenderer>(),
+            GameObject.Find("Up7").GetComponent<MeshRenderer>(), 
+            GameObject.Find("Up8").GetComponent<MeshRenderer>(), 
+            GameObject.Find("Up9").GetComponent<MeshRenderer>(),
+        };
+
+        down = new[]
+        {
+            GameObject.Find("Down1").GetComponent<MeshRenderer>(),
+            GameObject.Find("Down2").GetComponent<MeshRenderer>(),
+            GameObject.Find("Down3").GetComponent<MeshRenderer>(),
+            GameObject.Find("Down4").GetComponent<MeshRenderer>(),
+            GameObject.Find("Down5").GetComponent<MeshRenderer>(),
+            GameObject.Find("Down6").GetComponent<MeshRenderer>(),
+            GameObject.Find("Down7").GetComponent<MeshRenderer>(),
+            GameObject.Find("Down8").GetComponent<MeshRenderer>(),
+            GameObject.Find("Down9").GetComponent<MeshRenderer>(),
+        };
+
+        right = new[]
+        {
+            GameObject.Find("Right1").GetComponent<MeshRenderer>(),
+            GameObject.Find("Right2").GetComponent<MeshRenderer>(),
+            GameObject.Find("Right3").GetComponent<MeshRenderer>(),
+            GameObject.Find("Right4").GetComponent<MeshRenderer>(),
+            GameObject.Find("Right5").GetComponent<MeshRenderer>(),
+            GameObject.Find("Right6").GetComponent<MeshRenderer>(),
+            GameObject.Find("Right7").GetComponent<MeshRenderer>(),
+            GameObject.Find("Right8").GetComponent<MeshRenderer>(),
+            GameObject.Find("Right9").GetComponent<MeshRenderer>(),
+        };
+
+        left = new[]
+        {
+            GameObject.Find("Left1").GetComponent<MeshRenderer>(),
+            GameObject.Find("Left2").GetComponent<MeshRenderer>(),
+            GameObject.Find("Left3").GetComponent<MeshRenderer>(),
+            GameObject.Find("Left4").GetComponent<MeshRenderer>(),
+            GameObject.Find("Left5").GetComponent<MeshRenderer>(),
+            GameObject.Find("Left6").GetComponent<MeshRenderer>(),
+            GameObject.Find("Left7").GetComponent<MeshRenderer>(),
+            GameObject.Find("Left8").GetComponent<MeshRenderer>(),
+            GameObject.Find("Left9").GetComponent<MeshRenderer>(),
+        };
+
+        front = new[]
+        {
+            GameObject.Find("Front1").GetComponent<MeshRenderer>(),
+            GameObject.Find("Front2").GetComponent<MeshRenderer>(),
+            GameObject.Find("Front3").GetComponent<MeshRenderer>(),
+            GameObject.Find("Front4").GetComponent<MeshRenderer>(),
+            GameObject.Find("Front5").GetComponent<MeshRenderer>(),
+            GameObject.Find("Front6").GetComponent<MeshRenderer>(),
+            GameObject.Find("Front7").GetComponent<MeshRenderer>(),
+            GameObject.Find("Front8").GetComponent<MeshRenderer>(),
+            GameObject.Find("Front9").GetComponent<MeshRenderer>(),
+        };
+
+        back = new[]
+        {
+            GameObject.Find("Back1").GetComponent<MeshRenderer>(),
+            GameObject.Find("Back2").GetComponent<MeshRenderer>(),
+            GameObject.Find("Back3").GetComponent<MeshRenderer>(),
+            GameObject.Find("Back4").GetComponent<MeshRenderer>(),
+            GameObject.Find("Back5").GetComponent<MeshRenderer>(),
+            GameObject.Find("Back6").GetComponent<MeshRenderer>(),
+            GameObject.Find("Back7").GetComponent<MeshRenderer>(),
+            GameObject.Find("Back8").GetComponent<MeshRenderer>(),
+            GameObject.Find("Back9").GetComponent<MeshRenderer>(),
+        };
+        
         rightUp = InputSystem.actions.FindAction("RightUp");
         rightDown = InputSystem.actions.FindAction("RightDown");
         leftUp = InputSystem.actions.FindAction("LeftUp");
@@ -59,71 +134,112 @@ public class CubeController : MonoBehaviour
             lMap[i] = orange;
             rMap[i] = red;
         }
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (!working)
+        if (x.WasPressedThisFrame())
         {
-            if (x.WasPressedThisFrame() && !working)
+            X();
+        }
+        else if (xPrime.WasPressedThisFrame())
+        {
+            XPrime();
+        }
+        else if (y.WasPressedThisFrame())
+        {
+            Y();
+        }
+        else if (yPrime.WasPressedThisFrame())
+        {
+            YPrime();
+        }
+        else if (z.WasPressedThisFrame())
+        {
+            Z();
+        }
+        else if (zPrime.WasPressedThisFrame())
+        {
+            ZPrime();
+        }
+        else if (r1.WasPressedThisFrame())
+        {
+            if (rightUp.IsPressed())
             {
-                X();
+                U();
             }
-            else if (xPrime.WasPressedThisFrame() && !working)
+            else if (rightDown.IsPressed())
             {
-                XPrime();
-            }
-            else if (y.WasPressedThisFrame() && !working)
-            {
-                Y();
-            }
-            else if (yPrime.WasPressedThisFrame() && !working)
-            {
-                YPrime();
-            }
-            else if (z.WasPressedThisFrame() && !working)
-            {
-                Z();
-            }
-            else if (zPrime.WasPressedThisFrame() && !working)
-            {
-                ZPrime();
-            }
-            else if (r1.WasPressedThisFrame() && !working)
-            {
-                if (rightUp.IsPressed() && !working)
-                {
-                    U();
-                }
-                else if (rightDown.IsPressed() && !working)
-                {
 
-                }
             }
-            else if (l1.WasPressedThisFrame() && !working)
+        }
+        else if (r2.WasPressedThisFrame())
+        {
+            if (rightUp.IsPressed())
             {
-                if (leftUp.IsPressed() && !working)
-                {
-                    UPrime();
-                }
-                else if (leftDown.IsPressed() && !working)
-                {
-
-                }
+                EPrime();
             }
-
-            int i = 0;
-            while (i < 9)
+            else if (rightDown.IsPressed())
             {
-                up[i].material = uMap[i];
-                down[i].material = dMap[i];
-                front[i].material = fMap[i];
-                back[i].material = bMap[i];
-                left[i].material = lMap[i];
-                right[i].material = rMap[i];
-                i++;
+                
             }
+        }
+        else if (r3.WasPressedThisFrame())
+        {
+            if (rightUp.IsPressed())
+            {
+                DPrime();
+            }
+            else if (rightDown.IsPressed())
+            {
+                
+            }
+        }
+        else if (l1.WasPressedThisFrame())
+        {
+            if (leftUp.IsPressed())
+            {
+                UPrime();
+            }
+            else if (leftDown.IsPressed())
+            {
+
+            }
+        }
+        else if (l2.WasPressedThisFrame())
+        {
+            if (leftUp.IsPressed())
+            {
+                E();
+            }
+            else if (leftDown.IsPressed())
+            {
+                
+            }
+        }
+        else if (l3.WasPressedThisFrame())
+        {
+            if (leftUp.IsPressed())
+            {
+                D();
+            }
+            else if (leftDown.IsPressed())
+            {
+                
+            }
+        }
+            
+        int i = 0;
+        while (i < 9)
+        {
+            up[i].material = uMap[i];
+            down[i].material = dMap[i];
+            front[i].material = fMap[i];
+            back[i].material = bMap[i];
+            left[i].material = lMap[i];
+            right[i].material = rMap[i];
+            i++;
         }
     }
 
@@ -148,8 +264,6 @@ public class CubeController : MonoBehaviour
 
     void X()
     {
-        working = true;
-        
         Material[] uMapPrev = DeepCopy(uMap);
         Material[] dMapPrev = DeepCopy(dMap);
         Material[] fMapPrev = DeepCopy(fMap);
@@ -188,14 +302,10 @@ public class CubeController : MonoBehaviour
         };
         
         uMap = fMapPrev;
-        
-        working = false;
     }
 
     void XPrime()
     {
-        working = true;
-        
         Material[] uMapPrev = DeepCopy(uMap);
         Material[] dMapPrev = DeepCopy(dMap);
         Material[] fMapPrev = DeepCopy(fMap);
@@ -234,14 +344,10 @@ public class CubeController : MonoBehaviour
         };
         
         dMap = fMapPrev;
-        
-        working = false;
     }
 
     void Y()
     {
-        working = true;
-        
         Material[] uMapPrev = DeepCopy(uMap);
         Material[] dMapPrev = DeepCopy(dMap);
         Material[] fMapPrev = DeepCopy(fMap);
@@ -270,14 +376,10 @@ public class CubeController : MonoBehaviour
         bMap = lMapPrev;
         
         lMap = fMapPrev;
-        
-        working = false;
     }
 
     void YPrime()
     {
-        working = true;
-        
         Material[] uMapPrev = DeepCopy(uMap);
         Material[] dMapPrev = DeepCopy(dMap);
         Material[] fMapPrev = DeepCopy(fMap);
@@ -303,14 +405,10 @@ public class CubeController : MonoBehaviour
         lMap = bMapPrev;
         bMap = rMapPrev;
         rMap = fMapPrev;
-        
-        working = false;
     }
 
     void Z()
     {
-        working = true;
-        
         Material[] uMapPrev = DeepCopy(uMap);
         Material[] dMapPrev = DeepCopy(dMap);
         Material[] fMapPrev = DeepCopy(fMap);
@@ -359,13 +457,10 @@ public class CubeController : MonoBehaviour
             uMapPrev[7], uMapPrev[4], uMapPrev[1],
             uMapPrev[8], uMapPrev[5], uMapPrev[2],
         };
-        working = false;
     }
 
     void ZPrime()
     {
-        working = true;
-        
         Material[] uMapPrev = DeepCopy(uMap);
         Material[] dMapPrev = DeepCopy(dMap);
         Material[] fMapPrev = DeepCopy(fMap);
@@ -414,14 +509,10 @@ public class CubeController : MonoBehaviour
             dMapPrev[1], dMapPrev[4], dMapPrev[7],
             dMapPrev[0], dMapPrev[3], dMapPrev[6],
         };
-        
-        working = false;
     }
 
     void U()
     {
-        working = true;
-        
         Material[] uMapPrev = DeepCopy(uMap);
         Material[] dMapPrev = DeepCopy(dMap);
         Material[] fMapPrev = DeepCopy(fMap);
@@ -434,7 +525,6 @@ public class CubeController : MonoBehaviour
             uMapPrev[7], uMapPrev[4], uMapPrev[1],
             uMapPrev[8], uMapPrev[5], uMapPrev[2],
         };
-
         
         fMap[0] = rMapPrev[0];
         fMap[1] = rMapPrev[1];
@@ -451,13 +541,11 @@ public class CubeController : MonoBehaviour
         lMap[0] = fMapPrev[0];
         lMap[1] = fMapPrev[1];
         lMap[2] = fMapPrev[2];
-        
-        working = false;
     }
 
     void UPrime()
     {
-        working = true;
+        
         
         Material[] uMapPrev = DeepCopy(uMap);
         Material[] dMapPrev = DeepCopy(dMap);
@@ -488,7 +576,123 @@ public class CubeController : MonoBehaviour
         rMap[0] = fMapPrev[0];
         rMap[1] = fMapPrev[1];
         rMap[2] = fMapPrev[2];
+    }
+
+    void E()
+    {
+        Material[] uMapPrev = DeepCopy(uMap);
+        Material[] dMapPrev = DeepCopy(dMap);
+        Material[] fMapPrev = DeepCopy(fMap);
+        Material[] bMapPrev = DeepCopy(bMap);
+        Material[] lMapPrev = DeepCopy(lMap);
+        Material[] rMapPrev = DeepCopy(rMap);
         
-        working = false;
+        fMap[3] = lMapPrev[3];
+        fMap[4] = lMapPrev[4];
+        fMap[5] = lMapPrev[5];
+        
+        lMap[3] = bMapPrev[3];
+        lMap[4] = bMapPrev[4];
+        lMap[5] = bMapPrev[5];
+        
+        bMap[3] = rMapPrev[3];
+        bMap[4] = rMapPrev[4];
+        bMap[5] = rMapPrev[5];
+        
+        rMap[3] = fMapPrev[3];
+        rMap[4] = fMapPrev[4];
+        rMap[5] = fMapPrev[5];
+    }
+
+    void EPrime()
+    {
+        Material[] uMapPrev = DeepCopy(uMap);
+        Material[] dMapPrev = DeepCopy(dMap);
+        Material[] fMapPrev = DeepCopy(fMap);
+        Material[] bMapPrev = DeepCopy(bMap);
+        Material[] lMapPrev = DeepCopy(lMap);
+        Material[] rMapPrev = DeepCopy(rMap);
+        
+        fMap[3] = rMapPrev[3];
+        fMap[4] = rMapPrev[4];
+        fMap[5] = rMapPrev[5];
+        
+        rMap[3] = bMapPrev[3];
+        rMap[4] = bMapPrev[4];
+        rMap[5] = bMapPrev[5];
+        
+        bMap[3] = lMapPrev[3];
+        bMap[4] = lMapPrev[4];
+        bMap[5] = lMapPrev[5];
+        
+        lMap[3] = fMapPrev[3];
+        lMap[4] = fMapPrev[4];
+        lMap[5] = fMapPrev[5];
+    }
+
+    void D()
+    {
+        Material[] uMapPrev = DeepCopy(uMap);
+        Material[] dMapPrev = DeepCopy(dMap);
+        Material[] fMapPrev = DeepCopy(fMap);
+        Material[] bMapPrev = DeepCopy(bMap);
+        Material[] lMapPrev = DeepCopy(lMap);
+        Material[] rMapPrev = DeepCopy(rMap);
+
+        dMap = new[]
+        {
+            dMapPrev[6], dMapPrev[3], dMapPrev[0],
+            dMapPrev[7], dMapPrev[4], dMapPrev[1],
+            dMapPrev[8], dMapPrev[5], dMapPrev[2]
+        };
+        
+        fMap[6] = lMapPrev[6];
+        fMap[7] = lMapPrev[7];
+        fMap[8] = lMapPrev[8];
+        
+        lMap[6] = bMapPrev[6];
+        lMap[7] = bMapPrev[7];
+        lMap[8] = bMapPrev[8];
+        
+        bMap[6] = rMapPrev[6];
+        bMap[7] = rMapPrev[7];
+        bMap[8] = rMapPrev[8];
+        
+        rMap[6] = fMapPrev[6];
+        rMap[7] = fMapPrev[7];
+        rMap[8] = fMapPrev[8];
+    }
+
+    void DPrime()
+    {
+        Material[] uMapPrev = DeepCopy(uMap);
+        Material[] dMapPrev = DeepCopy(dMap);
+        Material[] fMapPrev = DeepCopy(fMap);
+        Material[] bMapPrev = DeepCopy(bMap);
+        Material[] lMapPrev = DeepCopy(lMap);
+        Material[] rMapPrev = DeepCopy(rMap);
+
+        dMap = new[]
+        {
+            dMapPrev[2], dMapPrev[5], dMapPrev[8],
+            dMapPrev[1], dMapPrev[4], dMapPrev[7],
+            dMapPrev[0], dMapPrev[3], dMapPrev[6]
+        };
+        
+        fMap[6] = rMapPrev[6];
+        fMap[7] = rMapPrev[7];
+        fMap[8] = rMapPrev[8];
+        
+        rMap[6] = bMapPrev[6];
+        rMap[7] = bMapPrev[7];
+        rMap[8] = bMapPrev[8];
+        
+        bMap[6] = lMapPrev[6];
+        bMap[7] = lMapPrev[7];
+        bMap[8] = lMapPrev[8];
+        
+        lMap[6] = fMapPrev[6];
+        lMap[7] = fMapPrev[7];
+        lMap[8] = fMapPrev[8];
     }
 }
